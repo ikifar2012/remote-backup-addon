e52184dd753ca0750407bd39059cc10ebc3766d6
patman15 <14628713+patman15@users.noreply.github.com>
2022-08-22 14:37:37 +0200

    coding style fixes

--- remote-backup/run.sh
+++ remote-backup/run.sh
@@ -34,16 +34,16 @@ BACKUP_NAME="${BACKUP_CUSTOM_PREFIX} $(date +'%Y-%m-%d %H-%M')"
 SSH_HOME="${HOME}/.ssh"
 
 function set-debug-level {
-  # default log level according to bashio const.sh is INFO
-  if bashio::var.true "${DEBUG}"; then
-    bashio::log.level "debug"
-  fi
+    # default log level according to bashio const.sh is INFO
+    if bashio::var.true "${DEBUG}"; then
+        bashio::log.level "debug"
+    fi
 }
 
 # Arguments:
 #   $1 result should be ok or error
 #   $2 message message to send with the event
-function fire_event {
+function fire-event {
     local result=${1}
     local message=${2:-}
 
@@ -61,7 +61,7 @@ function die {
     # catch return code which is always false, see https://github.com/hassio-addons/bashio/issues/31
     local ret=$(bashio::api.supervisor POST /core/api/services/persistent_notification/create \
         "{\"message\":\"${message}\", \"title\":\"${title}\", \"notification_id\":\"addon-remote-backup\"}")
-    fire_event "error" "${message}"
+    fire-event "error" "${message}"
     bashio::exit.nok "${message}"
 }
 
@@ -313,5 +313,5 @@ clone-to-remote || die "Cloning backup(s) to remote host ${REMOTE_HOST} failed! 
 delete-local-backup || die "Removing local backup(s) failed! See log for details."
 
 bashio::log.info "Backup process done!"
-fire_event "ok" "Backup ${BACKUP_NAME} created."
+fire-event "ok" "Backup ${BACKUP_NAME} created."
 bashio::exit.ok
